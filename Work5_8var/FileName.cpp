ac9b9a6417d6b5164696580c9ac16c0a3e671ccb
maks2337 <maksimzinovev13031@gmail.com>
2025-09-23 20:53:59 +0300

    Добавьте файлы проекта.

---
+++ Work5_8var/FileName.cpp
@@ -0,0 +1,16 @@
+﻿// ПРАКТИЧЕСКАЯ РАБОТА №5, Задание 2 (8 вариант) Написать программу, которая выводит таблицу степеней двойки от нулевой до десятой
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int number = 2;
+	cout << "Таблица степеней двойки:" << endl;
+	for (int i = 0; i <= 10; i++) {
+		cout << i << "     ";
+		cout << pow(number, i) << endl;
+	}
+}
